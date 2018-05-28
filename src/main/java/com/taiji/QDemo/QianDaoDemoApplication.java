57440cfa3900cf2014e7ca3acbdb5d2e75d4be8c
Demon07 <747576536@qq.com>
2018-05-28 14:42:09 +0800

    作业提交

---
+++ src/main/java/com/taiji/QDemo/QianDaoDemoApplication.java
@@ -0,0 +1,14 @@
+package com.taiji.QDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.taiji.QDemo.web.QianDaocontroller;
+
+@SpringBootApplication
+public class QianDaoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QianDaoDemoApplication.class, args);
+	}
+}
